727f0ec8b13095ebf61eed739d647f5d60f63701
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2022-09-11 19:37:32 +0200

    revert dockerfile changes

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,7 @@ COPY package*.json ./
 RUN npm install
  
 COPY . .
-
+ 
 EXPOSE 3000
  
-RUN npm run build
-
-CMD ["npx", "serve", "-s", "build"]
+CMD [ "npm", "start" ]
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(process.env.NODE_ENV);
+  console.log(process.env);
   useEffect(() => {
     const main = async () => {
       const res = await api
